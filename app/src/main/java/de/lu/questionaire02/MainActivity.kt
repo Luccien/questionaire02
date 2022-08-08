ff073c0792ec4033ba5d9d6978d9b4998c768819
lucie <lu@>
2022-08-08 12:34:12 +0200

    working on column placement

--- app/src/main/java/de/lu/questionaire02/MainActivity.kt
+++ app/src/main/java/de/lu/questionaire02/MainActivity.kt
@@ -2,21 +2,19 @@ package de.lu.questionaire02
 
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.res.painterResource
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -37,29 +35,67 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting()
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Greeting()
+                        //FavoriteCollectionCard()
+                    }
                 }
             }
         }
     }
 }
 
+
+
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
-Column(modifier) {
+Column(
+    horizontalAlignment = Alignment.CenterHorizontally,
+    modifier = modifier.fillMaxSize()) {
     Image(
         painter = painterResource(id = R.drawable.p1food ),
         contentDescription = null,
         contentScale = ContentScale.Crop,
-        modifier = Modifier.size(88.dp).clip(CircleShape)
+        modifier = Modifier
+            .size(88.dp)
+            .clip(CircleShape)
     )
     Text(
-        stringResource(id = R.string.windPower)
+        stringResource(id = R.string.windPower),
+        //style = MaterialTheme.typography.h3,
+        modifier = Modifier
+            .paddingFromBaseline(top = 24.dp, bottom = 8.dp)
     )
+    FavoriteCollectionCard()
+}
+
 }
 
 
+@Composable
+fun FavoriteCollectionCard(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()) {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row {
+                Image(
+                    painter = painterResource(id = R.drawable.p1),
+                    contentDescription = null
+                )
+                Text(
+                    stringResource(id = R.string.windPower)
+                )
+            }
+
+        }
+    }
 }
+
+
+
 
 @Preview(showBackground = true)
 @Composable
